344c19e0394d184f044c8e2259a35ceed753c8f2
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-12-31 20:15:56 +0200

    first commit

---
+++ src/main/java/org/example/civic_action/CivicActionApplication.java
@@ -0,0 +1,12 @@
+package org.example.civic_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivicActionApplication.class, args);
+    }
+}
---
+++ src/test/java/org/example/civic_action/CivicActionApplicationTests.java
@@ -0,0 +1,12 @@
+package org.example.civic_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
