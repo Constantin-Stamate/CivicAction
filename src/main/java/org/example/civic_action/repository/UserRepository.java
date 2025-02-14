6b30db8adc4b41e7fec00c1cc87345c143e74508
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-02-14 18:59:23 +0200

    add UserRepository interface with findByEmail method

---
+++ src/main/java/org/example/civic_action/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.example.civic_action.repository;
+
+import org.example.civic_action.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
