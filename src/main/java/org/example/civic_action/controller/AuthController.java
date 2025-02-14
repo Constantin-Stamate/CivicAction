94ac2f1a9299ef6da8e255251af21c6cb1be6f54
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-02-14 19:17:31 +0200

    add registration and login functionality with validation and user persistence

---
+++ src/main/java/org/example/civic_action/controller/AuthController.java
@@ -0,0 +1,51 @@
+package org.example.civic_action.controller;
+
+import jakarta.validation.Valid;
+import org.example.civic_action.dto.UserDto;
+import org.example.civic_action.entity.User;
+import org.example.civic_action.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
+                               BindingResult result,
+                               Model model) {
+        User existingUser = userService.findByEmail(userDto.getEmail());
+
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+            result.rejectValue("email", null, "There is already an account registered with that email");
+        }
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
+            return "redirect:/register?error";
+        }
+        userService.saveUser(userDto);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("page", "login");
+        return "login";
+    }
+}
