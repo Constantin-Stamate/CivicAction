f138e3fec88ded04444c81763100a2575af1c38f
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-02-14 18:56:27 +0200

    add RoleRepository interface with findByName method

---
+++ src/main/java/org/example/civic_action/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package org.example.civic_action.repository;
+
+import org.example.civic_action.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
