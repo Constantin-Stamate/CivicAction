2e4d68cea73aab6bbf14fe2a102e7903349f752a
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-02-17 14:56:32 +0200

    add law submission handling and user image assignment in results view

--- src/main/java/org/example/civic_action/controller/NavigationController.java
+++ src/main/java/org/example/civic_action/controller/NavigationController.java
@@ -1,11 +1,19 @@
 package org.example.civic_action.controller;
 
+import org.example.civic_action.entity.Law;
+import org.example.civic_action.service.LawService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class NavigationController {
+
+    @Autowired
+    private LawService lawService;
 
     @GetMapping("/index")
     public String index(Model model) {
@@ -33,7 +41,12 @@ public class NavigationController {
 
     @GetMapping("/results")
     public String results(Model model) {
-        model.addAttribute("page", "results");
+        List<Law> lawsAll = lawService.getAllLaws();
+        for (Law law : lawsAll) {
+            String userImage = law.getUserGender().equals("Male") ? "/images/users/user_1.png" : "/images/users/user_2.png";
+            law.setUserImage(userImage);
+        }
+        model.addAttribute("lawsAll", lawsAll);
         return "results";
     }
 }
